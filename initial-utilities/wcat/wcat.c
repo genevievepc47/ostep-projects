d0fe85b2cf4b1621b1ead0e1167f4cb80dd8f0cf
Gen P Cohen <genevievepc47@gmail.com>
2020-01-15 16:07:44 -0500

    added multiple file handling

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 
+//git add .
+//git commit -m "commit message"
+//pit push
 int main(int argc, char *argv[])
 {
 
 //if argc is 1, they didnt enter a file same
 //argv[1]
-	FILE *fp = fopen("main.c", "r");
-	if (fp == NULL)
+
+
+	//check if they entered a file
+	if(argc == 1)//if they did not enter any files
 	{
-		printf("wcat:cannot open file\n");
-		exit(1);
+		printf("No files entered");
+		exit(0);
 	}
 
-	char buffer[1000];
-	while(fgets(buffer, sizeof buffer, fp) != NULL)//read the file one line at a time
+	int i;
+	for(i =2; i<= argc; i++)//loop through each file sent in
 	{
-		printf("%s", buffer);
-	}
+		FILE *fp = fopen(argv[i]", "r");
+		if (fp == NULL)
+		{
+			printf("wcat:cannot open file\n");
+			exit(1);
+		}
 
-	fclose(fp);
+		char buffer[1000];
+		while(fgets(buffer, sizeof buffer, fp) != NULL)//read the file one line at a time
+		{
+			printf("%s", buffer);
+		}
+
+		fclose(fp);
+	}//end for each file
 	exit(0);
 
 }//end main function
