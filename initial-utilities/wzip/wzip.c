07446170fb3866811c7d415cd7ca241a9c35b434
Gen P Cohen <genevievepc47@gmail.com>
2020-01-16 20:51:42 -0500

    started wzip

---
+++ initial-utilities/wzip/wzip.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//git add .
+//git commit -m "commit message"
+//pit push
+
+//CONFUSED ON HOW TO SET IT UP
+//HOW TO ACCEPT BOTH THE FILE ARGUMENTS
+//DO WE NEED TO WRITE IT TO STDOUT AND THE SECOND FILE? DIRECTIONS UNCLEAR
+int main(int argc, char *argv[])
+{
+
+//if argc is 1, they didnt enter a file same
+//argv[1]
+
+
+        //check if they entered a file
+        if(argc == 1)//if they did not enter any files
+        {
+                printf("No files entered");
+                exit(0);
+        }
+}//end main
